69420b32afd68b36883df95b57d658aa8bdd38fa
cristopfer <eysen.perez1@unmsm.edu.pe>
2024-12-05 22:02:37 -0500

    subiendo

--- models/autenticar.js
+++ models/autenticar.js
@@ -16,10 +16,9 @@ function IngresarSistemaUsuario(usuario, respuesta) {
                 respuesta.send(JSON.stringify({ estado: 0 })); // Usuario no encontrado
             } else {
                 const hashedPassword = data.rows[0].clave;
-				console.log(data.rows[0].clave);
                 bcrypt.compare(usuario.password, hashedPassword, function (err, result) {
                     if (result) {
-                        respuesta.send(JSON.stringify({ estado: 1 })); // Login exitoso
+                        respuesta.send(JSON.stringify({ estado: data.rows[0].tipo })); // Login exitoso
                     } else {
                         respuesta.send(JSON.stringify({ estado: 0 })); // Contraseña incorrecta
                     }
