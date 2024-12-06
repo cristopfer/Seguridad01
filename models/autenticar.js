173243ca5085d6846e9ea143437f543088748068
cristopfer <eysen.perez1@unmsm.edu.pe>
2024-12-05 19:34:59 -0500

    actualizando hash

--- models/autenticar.js
+++ models/autenticar.js
@@ -1,35 +1,55 @@
 var pool = require('./conexion').pool;
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-function Autenticar(requerimiento, respuesta, direccion, path){
+function Autenticar(requerimiento, respuesta, direccion, path) {
     respuesta.render(direccion + path);
 }
 
-function IngresarSistemaUsuario(usuario, respuesta){
-	pool.connect(function(err, client, done){		
-		client.query("SELECT * FROM ingresar_sistema($1,$2)", [usuario.username,usuario.password],function(err,data) {        
-        	done();				
-			if(err){ 	 
-        		LogModel.ErrorLog("models/autenticar", "ingresar_sistema", err.message);                
-        	}  
-        	else{ 		
-            	respuesta.send(JSON.stringify({ estado: data.rows[0].ingresar_sistema })); 
-        	}   
-    	});
-	});    
+function IngresarSistemaUsuario(usuario, respuesta) {
+    pool.connect(function (err, client, done) {
+        client.query("SELECT password FROM usuarios WHERE username = $1", [usuario.username], function (err, data) {
+            done();
+            if (err) {
+                LogModel.ErrorLog("models/autenticar", "ingresar_sistema", err.message);
+            } else if (data.rows.length === 0) {
+                respuesta.send(JSON.stringify({ estado: 0 })); // Usuario no encontrado
+            } else {
+                const hashedPassword = data.rows[0].password;
+                bcrypt.compare(usuario.password, hashedPassword, function (err, result) {
+                    if (result) {
+                        respuesta.send(JSON.stringify({ estado: 1 })); // Login exitoso
+                    } else {
+                        respuesta.send(JSON.stringify({ estado: 0 })); // Contraseña incorrecta
+                    }
+                });
+            }
+        });
+    });
 }
 
-function RegistrarUsuario(usuario, respuesta){
-	pool.connect(function(err, client, done){
-		client.query("SELECT * FROM registrar_usuario($1,$2,$3,$4,$5,$6,$7)", [usuario.name,usuario.lastname,usuario.username,usuario.mail,usuario.password,parseInt(usuario.tipo),usuario.codigo],function(err,data) {        
-        	done();				
-			if(err){ 	 
-        		LogModel.ErrorLog("models/autenticar", "registrar_usuario", err.message);                
-        	}  
-        	else{ 		
-            	respuesta.send(JSON.stringify({ estado: data.rows[0].registrar_usuario })); 
-        	}   
-    	});
-	});    
+function RegistrarUsuario(usuario, respuesta) {
+    bcrypt.hash(usuario.password, saltRounds, function (err, hash) {
+        if (err) {
+            LogModel.ErrorLog("models/autenticar", "hash_password", err.message);
+            respuesta.send(JSON.stringify({ estado: 0 }));
+        } else {
+            pool.connect(function (err, client, done) {
+                client.query(
+                    "SELECT * FROM registrar_usuario($1,$2,$3,$4,$5,$6,$7)",
+                    [usuario.name, usuario.lastname, usuario.username, usuario.mail, hash, parseInt(usuario.tipo), usuario.codigo],
+                    function (err, data) {
+                        done();
+                        if (err) {
+                            LogModel.ErrorLog("models/autenticar", "registrar_usuario", err.message);
+                        } else {
+                            respuesta.send(JSON.stringify({ estado: data.rows[0].registrar_usuario }));
+                        }
+                    }
+                );
+            });
+        }
+    });
 }
 
 module.exports.Autenticar = Autenticar;
